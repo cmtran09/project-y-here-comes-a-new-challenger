5c16528f7b3dc596db9f6b33556dfd64a1383190
cmtran09 <cmtran@hotmail.co.uk>
2020-07-30 19:39:21 +0100

    🛠 Apollo provider added to client tools

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,35 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import ReactDOM from 'react-dom'
 
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloProvider } from '@apollo/react-hooks'
+
 import './styles/styles.scss'
 
 import Home from './components/Home/Home'
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5000/'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
 const App = () => {
   return (
-    <BrowserRouter>
-      {/* <Header /> */}
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider>
+
   )
 }
 
