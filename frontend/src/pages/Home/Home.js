0abaa941996c45dccbb5ea57ef54e48253eb7459
cmtran09 <cmtran@hotmail.co.uk>
2020-08-10 23:16:00 +0100

    card dummy

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 
 export default function Home() {
@@ -15,6 +16,24 @@ export default function Home() {
   return (
     <div>
       <h1>hello world HOme</h1>
+      <Card>
+        <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+          <Card.Meta>
+            <span className='date'>Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            Matthew is a musician living in Nashville.
+      </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name='user' />
+        22 Friends
+      </a>
+        </Card.Content>
+      </Card>
     </div>
   )
 }
